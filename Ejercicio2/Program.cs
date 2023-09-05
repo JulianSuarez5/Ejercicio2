ac05fccecebb752b6f375064912ae3a6e188cfb4
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:25:50 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class OperacionesBasicas
+{
+    static void Main()
+    {
+        int num1, num2, resultado;
+        string operacion;
+
+        Console.Write("Ingrese el primer número: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        num2 = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese la operación (+, -, *, /): ");
+        operacion = Console.ReadLine();
+
+        switch (operacion)
+        {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            default:
+                resultado = 0;
+                break;
+        }
+
+        Console.WriteLine("El resultado es: " + resultado);
+        Console.ReadKey();
+    }
+}
